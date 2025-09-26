77cf546de906693f55f443644288d233f6a773d5
Georg Heiler <georg.kf.heiler@gmail.com>
2025-09-26 10:49:05 +0200

    k

--- {{ cookiecutter.project_slug }}/src/code_location_foo/quickstart_etl/defs/hn/component.py
+++ {{ cookiecutter.project_slug }}/src/code_location_foo/quickstart_etl/defs/hn/component.py
@@ -1,9 +1,24 @@
 import dagster as dg
 
+daily_refresh_schedule = dg.ScheduleDefinition(
+    job=dg.define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
+)
+
+
+@dg.op
+def foo_op():
+    return 5
+
+
+@dg.graph_asset
+def my_asset():
+    return foo_op()
+
+
 
 class HnComponent(dg.Component, dg.Model, dg.Resolvable):
     def build_defs(self, context: dg.ComponentLoadContext) -> dg.Definitions:
-        return dg.Definitions()
+        return dg.Definitions(jobs=[daily_refresh_schedule], assets=[my_asset()])
 
 
 @dg.component_instance
---
+++ {{ cookiecutter.project_slug }}/src/code_location_{{ cookiecutter.project_slug }}/code_location_{{ cookiecutter.project_slug }}/definitions.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import dagster as dg
+from .resources import get_resources_for_deployment
+
+@dg.definitions
+def defs():
+    root_dir = __file__
+    resource_defs = get_resources_for_deployment()
+    base = dg.load_from_defs_folder(path_within_project=Path(root_dir).parent.parent)
+    assets_with_eager = dg.with_attributes(
+    base,
+    automation_condition=dg.AutomationCondition.eager()
+)
+    merged_defs = dg.Definitions.merge(
+        assets_with_eager, dg.Definitions(resources=resource_defs)
+    )
+
+    materializable = [a for a in merged_defs.assets if isinstance(a, dg.AssetsDefinition)]  # type: ignore
+    passthrough = [
+        a
+        for a in merged_defs.assets  # type: ignore
+        if not isinstance(a, dg.AssetsDefinition)
+    ]  # SourceAsset
+
+    with_refs = dg.with_source_code_references(materializable)
+
+    linked = dg.link_code_references_to_git(
+        assets_defs=with_refs,
+        git_url="https://github.com/{{ cookiecutter.organization }}/{{ cookiecutter.project_slug }}/",
+        git_branch="main",
+        file_path_mapping=dg.AnchorBasedFilePathMapping(
+            local_file_anchor=Path(root_dir).parent,
+            file_anchor_path_in_repository="src/code_location_{{ cookiecutter.project_slug }}",
+        ),
+    )
+
+    return dg.Definitions(
+        assets=[*linked, *passthrough],
+        jobs=base.jobs,
+        schedules=base.schedules,
+        sensors=base.sensors,
+        resources=base.resources,
+        asset_checks=base.asset_checks,
+    )

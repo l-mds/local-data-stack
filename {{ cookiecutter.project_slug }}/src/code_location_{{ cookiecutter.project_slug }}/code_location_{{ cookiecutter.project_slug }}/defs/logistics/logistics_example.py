f1f4fefe02313873cef48485c789b68bd8c19508
Georg Heiler <georg.kf.heiler@gmail.com>
2025-09-27 08:35:38 +0200

    k

--- {{ cookiecutter.project_slug }}/src/code_location_{{ cookiecutter.project_slug }}/code_location_{{ cookiecutter.project_slug }}/defs/logistics/logistics_example.py
+++ {{ cookiecutter.project_slug }}/src/code_location_{{ cookiecutter.project_slug }}/code_location_{{ cookiecutter.project_slug }}/defs/logistics/logistics_example.py
@@ -3,7 +3,8 @@ import dagster as dg
 @dg.asset(
     compute_kind="python",
     group_name="logistics",
-    deps=[dg.AssetKey(["analytics_database_dev", "bar_dev", "my_first_dbt_model_v1"])],
+    # deps=[dg.AssetKey(["analytics_database_dev", "bar_dev", "my_first_dbt_model_v1"])],
+    deps=[dg.AssetKey(["my_first_dbt_model_v1"])],
 )
 def logistics_example():
     return 1
